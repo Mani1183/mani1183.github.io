f5969521b3bc7ce13248f2a5e2f7cdff713df4ea
Manish K Sharma <76739229+manishk-sharma@users.noreply.github.com>
2026-02-05 23:40:48 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -69,8 +69,8 @@ export default function Contact() {
                   </div>
                   <div>
                     <p className="text-gray-400 text-base mb-1">Phone</p>
-                    <a href="tel:+91 XXXXXXXXXX" className="text-lg font-semibold hover:text-blue-500 transition-colors">
-                      +91 XXXXXXXXXX
+                    <a href="tel:+91 9990830656" className="text-lg font-semibold hover:text-blue-500 transition-colors">
+                      +91 9990830656
                     </a>
                   </div>
                 </div>
@@ -131,7 +131,7 @@ export default function Contact() {
                   <ion-icon name="mail-outline"></ion-icon>
                   Send me an email
                 </a>
-                <a href="tel:+91XXXXXXXXXX" className="w-full py-3 rounded-xl flex items-center justify-center gap-2 border border-white/10 text-white hover:bg-white/5 transition-colors text-lg font-medium">
+                <a href="tel:+919990830656" className="w-full py-3 rounded-xl flex items-center justify-center gap-2 border border-white/10 text-white hover:bg-white/5 transition-colors text-lg font-medium">
                   <ion-icon name="call-outline"></ion-icon>
                   Give me a call
                 </a>
@@ -144,7 +144,7 @@ export default function Contact() {
               <p className="text-gray-400 text-base mb-6">
                 Download my resume to see my complete work history and projects.
               </p>
-              <a href="https://drive.google.com/file/d/1iIiQWdw6pLqsQDLZ59Q3tjnUk5KUFiA4/view?usp=sharing"
+              <a href="https://drive.google.com/file/d/1vA9y8oSKX_vob0FpwXPxl-z46zR5XjCS/view?usp=sharing"
                 target="_blank" download className="w-full py-3 rounded-xl flex items-center justify-center gap-2 border border-white/10 text-white hover:bg-white/5 transition-colors bg-blue-700 hover:bg-blue-800">
                 <ion-icon name="download-outline"></ion-icon>
                 Download Resume
@@ -179,7 +179,7 @@ export default function Contact() {
                     name="email"
                     type="email"
                     className="input"
-                    placeholder="abc@example.com"
+                    placeholder="your@example.com"
                     required
                   />
                 </div>
